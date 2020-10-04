64b1030a22e6c91a7172f1042713bcb354d02067
남궁성환 <saturn203@naver.com>
2020-10-04 13:57:54 +0900

    redis 적용

--- src/main/java/com/example/common/controller/CommonApiController.java
+++ src/main/java/com/example/common/controller/CommonApiController.java
@@ -2,11 +2,14 @@ package com.example.common.controller;
 
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import com.example.common.model.Code;
 import com.example.common.model.ResultMessage;
 import com.example.common.repository.CommonRepository;
 
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,19 +18,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/api/common")
 @RequiredArgsConstructor
+@Slf4j
 public class CommonApiController {
     private final CommonRepository repository;
     private final static String CATEGORY_TYPE = "T01";
     private final static String MODEL_TYPE = "T02";
+    private final RedisTemplate<String, Object> redisTemplate;
 
     @GetMapping("/categories")
+    
     public List<Code> searchCategoryAll(){
+        log.info("============check1==searchCategoryAll====");
+        ValueOperations<String, Object> vop = redisTemplate.opsForValue();
+        List<Code> categoryList  = (List<Code>) vop.get("common::category-all");
+        if(categoryList !=null){
+            log.info("============check2==redis-hit====");
+            return categoryList;
+        }
+        categoryList = repository.findByCodeType(CATEGORY_TYPE);
 
-        List<Code> categoryList = repository.findByCodeType(CATEGORY_TYPE);
+        log.info("============check3==db-search====");
+
+        vop.set("common::category-all", categoryList, 1, TimeUnit.MINUTES);
         return categoryList;
 
     }
@@ -41,8 +58,16 @@ public class CommonApiController {
 
     @GetMapping("/models")
     public List<Code> searchModelAll(){
-
-        List<Code> modelList = repository.findByCodeType(MODEL_TYPE);
+        log.info("============check1==searchModelAll====");
+        ValueOperations<String, Object> vop = redisTemplate.opsForValue();
+        List<Code> modelList  = (List<Code>) vop.get("common::model-all");
+        if(modelList !=null){
+            log.info("============check2==cache hitttttt====");
+            return modelList;
+        }
+        log.info("============check3==search DB ====");
+        modelList = repository.findByCodeType(MODEL_TYPE);
+        vop.set("common::model-all", modelList, 1, TimeUnit.MINUTES);
         return modelList;
 
     }
