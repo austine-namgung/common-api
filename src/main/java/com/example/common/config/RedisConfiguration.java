460c077a2f3256dadf5ec18b329087e465145a73
Jaesung Lim <stuie.lim@osckorea.com>
2020-10-19 20:38:09 +0900

    k8s 에서 돌도록 설정

--- src/main/java/com/example/common/config/RedisConfiguration.java
+++ src/main/java/com/example/common/config/RedisConfiguration.java
@@ -1,5 +1,8 @@
 package com.example.common.config;
 
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -8,11 +11,20 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@Slf4j
 public class RedisConfiguration {
-    
+
+    @Value("${spring.redis.host}")
+    String host;
+
+    @Value("${spring.redis.port}")
+    Integer port;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory();
+        log.info("@@@@@ redis host = {}, port = {}", host,port);
+
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(host,port);
         return lettuceConnectionFactory;
     }
     @Bean
@@ -22,6 +34,6 @@ public class RedisConfiguration {
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         return redisTemplate;
     }
- 
+
 }
 
